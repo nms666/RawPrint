5c783792c41e3e59204fd13faf59edce8df20f0e
Tony Edgecombe <tonyedgecombe@gmail.com>
2014-12-16 16:57:55 +0000

    Updates to version info

--- RawPrint/Properties/AssemblyInfo.cs
+++ RawPrint/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RawPrint")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Raw Print")]
+[assembly: AssemblyDescription("Send raw print data directly to printer bypassing the driver.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RawPrint")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Frogmore Computer Services Ltd")]
+[assembly: AssemblyProduct("Raw Print")]
+[assembly: AssemblyCopyright("Copyright © Frogmore Computer Services Ltd 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,15 +21,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3839c608-4daa-4a61-b4f9-bec9b6393e3c")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
