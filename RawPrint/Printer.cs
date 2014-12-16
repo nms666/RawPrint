9548f0515739bf741de2dbd7aa10b66422a23f15
Tony Edgecombe <tonyedgecombe@gmail.com>
2014-12-16 16:55:11 +0000

    Initial commit

---
+++ RawPrint/Printer.cs
@@ -0,0 +1,15 @@
+﻿namespace RawPrint
+{
+    public class Printer
+    {
+        public static void PrintFile(string printer, string path, string documentName)
+        {
+            
+        }
+
+        public static void Printfile(string printer, string path)
+        {
+            PrintFile(printer, path, path);
+        }
+    }
+}
